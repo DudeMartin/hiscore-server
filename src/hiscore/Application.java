058a6d0d0203dbfba0f55d677f3ec633192a5382
Martin Tuskevicius <martin.tuskevicius@gmail.com>
2019-01-07 20:35:25 -0800

    The storage directory can now be specified by command-line arguments.

--- src/hiscore/Application.java
+++ src/hiscore/Application.java
@@ -12,11 +12,15 @@ public class Application {
 
     public static void main(String[] arguments) throws IOException {
         int port = 80;
+        String storagePath = System.getProperty("user.dir");
         if (arguments.length > 0) {
             port = Integer.parseInt(arguments[0]);
+            if (arguments.length > 1) {
+                storagePath = arguments[1];
+            }
         }
         System.out.println("Starting HTTP server on port " + port + "...");
-        RecordAccessor recordAccessor = new FileRecordAccessor(Paths.get(System.getProperty("user.dir")));
+        RecordAccessor recordAccessor = new FileRecordAccessor(Paths.get(storagePath));
         HttpServer httpServer = new NioHttpServer(recordAccessor);
         httpServer.start(port);
         Runtime.getRuntime().addShutdownHook(new Thread(() -> {
